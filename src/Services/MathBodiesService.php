7443b14037032a4851c3a73a7240cc99c91c404f
vdekovic <vedrandekovic@gmail.com>
2019-01-29 12:13:21 +0100

    Service minor change

--- src/Services/MathBodiesService.php
+++ src/Services/MathBodiesService.php
@@ -5,23 +5,23 @@ namespace App\Services;
 use App\Entities\Circle;
 use App\Entities\Triangle;
 
-class MathBodiesService
-{
+class MathBodiesService {
 
-    public static function getCircleSurface(Circle $circle) {
-
+    public static function getCircleSurface(Circle &$circle) {
+        return pi() * pow($circle->getRadius(), 2);
     }
 
-    public static function getCircleCircumference(Circle $circle) {
-
+    public static function getCircleCircumference(Circle &$circle) {
+        return 2 * pi() * $circle->getRadius();
     }
 
-    public static function getTriangleSurface(Triangle $triangle) {
-
+    public static function getTriangleSurface(Triangle &$triangle) {
+        $s = ($triangle->getA() + $triangle->getB() + $triangle->getC()) / 2;
+        $surface = sqrt($s*($s - $triangle->getA())*($s - $triangle->getB())*($s - $triangle->getC()));
+        return $surface;
     }
 
-    public static function getTriangleCircumference(Triangle $triangle) {
-
+    public static function getTriangleCircumference(Triangle &$triangle) {
+        return $triangle->getA() + $triangle->getB() + $triangle->getC();
     }
-
 }
