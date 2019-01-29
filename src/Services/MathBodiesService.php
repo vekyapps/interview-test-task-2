290524a5b9849efaa3be96184efa3a6c6528c933
vdekovic <vedrandekovic@gmail.com>
2019-01-29 11:37:52 +0100

    MathBodiesService added

---
+++ src/Services/MathBodiesService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\Circle;
+use App\Entities\Triangle;
+
+class MathBodiesService
+{
+
+    public static function getCircleSurface(Circle $circle) {
+
+    }
+
+    public static function getCircleCircumference(Circle $circle) {
+
+    }
+
+    public static function getTriangleSurface(Triangle $triangle) {
+
+    }
+
+    public static function getTriangleCircumference(Triangle $triangle) {
+
+    }
+
+}
